933172dcc34ed7f46c4fdcd133faad8799de3b50
gustavo henrique <gustavooalexandre1010@gmail.com>
2025-03-18 10:58:42 -0300

    meu primeiro projeto

---
+++ google.js
@@ -0,0 +1,8 @@
+document.getElementById('estrela').addEventListener("click", function(){
+    if(this.style.color== "blue") {
+        this.style.borderColor= "black";
+    } else {
+    this.style.color= "blue";
+    }
+
+})
